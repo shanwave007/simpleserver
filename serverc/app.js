84bc599ee5c9e84d3be99d7822ca0e54ceab2055
shanwave007 <shanwave007@gmail.com>
2021-01-31 15:56:33 +0530

    first commit

---
+++ serverc/app.js
@@ -0,0 +1,12 @@
+const { RSA_NO_PADDING } = require('constants');
+const http = require('http');
+
+
+const server = http.createServer((req, res)=>{
+    console.log(req);
+    res.setHeader('Content-type','text/html');
+    res.write("<html><body><h1>Hello 8083 third</h1></body></html>")
+    res.end();
+});
+
+server.listen(8080);
